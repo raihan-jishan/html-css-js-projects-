eb73e7345ce9d72d45c6d75a702898a87316a72a
Raihan <Raihan jishan@gmail.com>
2023-02-01 18:56:13 +0600

    first commit

--- script.js
+++
@@ -1,9 +0,0 @@
-//  require id my name
-let div = document.getElementById("time");
-let content = document.getElementById("content");
-const handleClick = () => {
-    return(
-    div.innerHTML = new Date(),
-    content.style.display = "none"
-    )
-};
